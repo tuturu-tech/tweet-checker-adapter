b036a36c99c3cfd2a02d969e59cd7600f39ece49
Elvis <elvis.skozdopolj@gmail.com>
2021-10-23 23:27:17 +0200

    first commit

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-const createRequest = require('./index').createRequest
+const createRequest = require("./index").createRequest;
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const port = process.env.EA_PORT || 8080
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const cors = require("cors");
+const port = process.env.EA_PORT || 8080;
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use(cors());
 
-app.post('/', (req, res) => {
-  console.log('POST Data: ', req.body)
+app.post("/", (req, res) => {
+  console.log("POST Data: ", req.body);
   createRequest(req.body, (status, result) => {
-    console.log('Result: ', result)
-    res.status(status).json(result)
-  })
-})
+    console.log("Result: ", result);
+    res.status(status).json(result.data.result);
+  });
+});
 
-app.listen(port, () => console.log(`Listening on port ${port}!`))
+app.listen(port, () => console.log(`Listening on port ${port}!`));
